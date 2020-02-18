893d9a0d33551dfe10a469e3242394ef81a9ca87
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-18 12:44:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,13 +10,12 @@ end
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language
 
-def introduction_with_language_optional (name, language = "Ruby")
+
+def introduction_with_language_optional (name, language= "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional
 
 
 
